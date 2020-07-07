abb125991c68978e4045615835bc59487d280404
Lycanthoss <Rokas.Rokanas@gmail.com>
2020-07-07 23:52:35 +0300

    Updates

---
+++ precache-manifest.80f7faccfc2f6aeea7096203ec376ce5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6582b309bcfb8322050ce843e3086a3",
+    "url": "/Weather-API/index.html"
+  },
+  {
+    "revision": "c0dfc422868c1f13a424",
+    "url": "/Weather-API/static/css/main.feb16de6.chunk.css"
+  },
+  {
+    "revision": "4fb6cfb194754beed521",
+    "url": "/Weather-API/static/js/2.60ca2bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-API/static/js/2.60ca2bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dfc422868c1f13a424",
+    "url": "/Weather-API/static/js/main.c6037607.chunk.js"
+  },
+  {
+    "revision": "10fc54a6fc445cdd826e",
+    "url": "/Weather-API/static/js/runtime-main.5e36e334.js"
+  },
+  {
+    "revision": "b0cf30aa5b653bdad27c2c6b920ab5bc",
+    "url": "/Weather-API/static/media/breakthrough.b0cf30aa.svg"
+  },
+  {
+    "revision": "2941f7a83b459cbc34fb3fea7782cf2f",
+    "url": "/Weather-API/static/media/cloudy.2941f7a8.svg"
+  },
+  {
+    "revision": "922f1f6ed126efda49cf89365f4fb75c",
+    "url": "/Weather-API/static/media/sunny.922f1f6e.svg"
+  }
+]);
